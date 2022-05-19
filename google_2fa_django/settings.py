92f353f788070ba1a370feed4cd8fc22cd292a1d
Aback231 <andrija231@gmail.com>
2022-05-19 12:18:59 +0200

    Update settings.py

--- google_2fa_django/settings.py
+++ google_2fa_django/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-i%a67w9l(bzc^qw#no=(+-cnkdeqc6ef!b97n##rsadb=$bf!3'
+SECRET_KEY = 'SECRET'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
